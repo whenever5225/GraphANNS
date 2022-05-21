65e5d50878f0951d55c5a376ddfb33ec039a5636
whenever5225 <156646264@qq.com>
2022-05-21 19:49:31 +0800

    add nndescent

---
+++ src/components/c1_initialization/implementation/c1_initialization_nndescent.h
@@ -0,0 +1,18 @@
+/***************************
+@Author: wmz
+@Contact: wmengzhao@qq.com
+@File: c1_initialization_nndescent.h
+@Time: 2022/5/21 7:20 PM
+@Desc: initialize a graph index with NNDescent (like 'NSG' algorithm)
+***************************/
+
+#ifndef GRAPHANNS_C1_INITIALIZATION_NNDESCENT_H
+#define GRAPHANNS_C1_INITIALIZATION_NNDESCENT_H
+
+#include "../c1_initialization_basic.h"
+
+class C1InitializationNNDescent : public C1InitializationBasic {
+
+};
+
+#endif //GRAPHANNS_C1_INITIALIZATION_NNDESCENT_H
