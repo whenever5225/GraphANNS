ccbc3b0dcbbc9069a7e3c144ba10248549708bea
whenever5225 <156646264@qq.com>
2022-10-01 17:00:06 +0800

    feat: add attribute filtering

---
+++ src/data_objects/distances/hamming_distance.h
@@ -0,0 +1,12 @@
+/***************************
+@Author: wmz
+@Contact: wmengzhao@qq.com
+@File: hamming_distance.h
+@Time: 2022/9/30 8:59 PM
+@Desc: TODO
+***************************/
+
+#ifndef GRAPHANNS_HAMMING_DISTANCE_H
+#define GRAPHANNS_HAMMING_DISTANCE_H
+
+#endif //GRAPHANNS_HAMMING_DISTANCE_H
