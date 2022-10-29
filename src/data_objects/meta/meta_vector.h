e38dedc17bd8298a9a41ef7b9731970a5a842a11
whenever5225 <156646264@qq.com>
2022-10-29 16:41:08 +0800

    feat: add query delete, multiple equal results support

---
+++ src/data_objects/meta/meta_vector.h
@@ -0,0 +1,12 @@
+/***************************
+@Author: wmz
+@Contact: wmengzhao@qq.com
+@File: meta_vector.h
+@Time: 2022/10/29 11:39 AM
+@Desc: TODO
+***************************/
+
+#ifndef GRAPHANNS_META_VECTOR_H
+#define GRAPHANNS_META_VECTOR_H
+
+#endif //GRAPHANNS_META_VECTOR_H
---
+++ src/elements/nodes/data_nodes/save_result_node.h
@@ -0,0 +1,12 @@
+/***************************
+@Author: wmz
+@Contact: wmengzhao@qq.com
+@File: save_result_node.h
+@Time: 2022/10/27 7:03 PM
+@Desc: TODO
+***************************/
+
+#ifndef GRAPHANNS_SAVE_RESULT_NODE_H
+#define GRAPHANNS_SAVE_RESULT_NODE_H
+
+#endif //GRAPHANNS_SAVE_RESULT_NODE_H
